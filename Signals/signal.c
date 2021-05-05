ce0e7e5917938be9d7001daa66c745e8485ddea9
Basantloay <basantloay2012@gmail.com>
2021-05-05 20:51:53 +0200

    Modify signals code

--- Signals/signal.c
+++ Signals/signal.c
@@ -9,10 +9,10 @@
 #define children 2
 
 int value;
-int pid,stat_loc;
+int pid;
 int n;
-int *pidArr;
-
+int *pidArr,*stat_loc;
+int flag1;
 void USRhandler(int signum);
 
 
@@ -20,15 +20,13 @@ void main(int argc,char**argv){
 	if(argc>2){
 		value=atoi(argv[1]);
 		//printf("%d\n",value);
-		int flag=1;
-		int flag1=0;
-		int flag2=0;
 		n=argc-2;
 		int*arr=malloc(sizeof(int)*n);
 		for(int j=0;j<n;j++)
   		arr[j]=atoi(argv[j+2]);
 		//printf("%d	%d	%d",arr[0],arr[2],arr[7]);
 		pidArr=malloc(sizeof(int)*3);
+		stat_loc=malloc(sizeof(int)*children);
 		for(int z=0;z<children+1;z++)
 			pidArr[z]=0;
 		signal(SIGUSR1,USRhandler);	  		
@@ -36,45 +34,24 @@ void main(int argc,char**argv){
 		printf("\nI am the parent,PID = %d\n",getpid());
 		pidArr[children+1]=getpid();  	  
 		
-		
-			(pidArr[1]=fork())&&(pidArr[0]=fork());
-			if (pid== -1)
-	  				perror("error in fork");
-	  		else if(pidArr[1] == 0) //child 2
-			 		{
-			 		flag=0;
-			 			pidArr[1]=getpid();
-			 			printf("\nI am the second child,PID = %d, PPID = %d \n",getpid(),getppid());
-			 			for(int k=n/2;k<n;k++)
-			 			{
-			 				if(arr[k]==value)
-			 				{
-			 					sleep(1);
-			 					kill(getppid(),SIGUSR1);
-			 					exit(k);
-			 					flag2=1;
-			 					}	
-			 			}
-			 			sleep(3);
-			 			printf("\nChild 2 terminates\n");
-			 			exit(0);
-			 		
-			 		}
-	  		else if (pidArr[0] == 0) //child 1
+		for(int i=0;i<children;i++)
+		{
+	  		 if (fork() == 0) //children
 			 	{ 
-			 	flag=1;
-			 	pidArr[0]=getpid();
 			 	
+			 	if (i==0) //child 1
+			 	{ 
+				pidArr[0]=getpid();
 	 			printf("\nI am the first child,PID = %d, PPID = %d \n",getpid(),getppid());
 			 		for(int k=0;k<n/2;k++)
 			 			{
 			 				if(arr[k]==value)
 			 					{
-			 					sleep(1);
+			 					sleep(0.000000001);
 			 					kill(getppid(),SIGUSR1);
 			 					
 			 					exit(k);
-			 					flag1=1;
+			 					
 			 					}	
 			 			}
 			 			sleep(3);
@@ -82,45 +59,21 @@ void main(int argc,char**argv){
 			 			exit(0);
 			 		}
 			 		
-			 		else
-			 			{
-			 			sleep(5);
-	pid= wait(&stat_loc);
-	if(!(stat_loc & 0x00FF))
-	{
-			printf("\nA child with pid %d terminated with exit code %d\n", pidArr[0], stat_loc>>8);
-		printf("\nA child with pid %d terminated with exit code %d\n", pidArr[1], stat_loc>>8);	
-		printf("Value not found\n");
-		exit(0);
-	}
-			 			}
-		
-		/*if (pidArr[0] == getpid()) //child 1
-			 	{ 
-			 	
-			 		for(int k=0;k<n/2;k++)
-			 			{
-			 				if(arr[k]==value)
-			 					{
-			 					kill(getppid(),SIGUSR1);
-			 					exit(k);
-			 					
-			 					}	
-			 			}
-			 			sleep(3);
-			 			printf("\nChild 1 terminates\n");
-			 			exit(0);
-			 		}
-			 		else if(pidArr[1] == getpid()) //child 2
-			 		{
+	  		
+			  if(i==1){
+			 			pidArr[1]=getpid();
 			 			
+			 			//printf("%d",pidArr[1]);
+			 			sleep(0.000000001);
+			 			printf("\nI am the second child,PID = %d, PPID = %d \n",getpid(),getppid());
 			 			for(int k=n/2;k<n;k++)
 			 			{
 			 				if(arr[k]==value)
 			 				{
+			 					sleep(0.001);
 			 					kill(getppid(),SIGUSR1);
 			 					exit(k);
-			 					flag2=1;
+			 					
 			 					}	
 			 			}
 			 			sleep(3);
@@ -128,20 +81,38 @@ void main(int argc,char**argv){
 			 			exit(0);
 			 		
 			 		}
-			*/
-	
-}
+			 		} 
+			 }
+			 sleep(5);
+			 for(int i=0;i<children;i++)
+			 {
+			pid= wait(&stat_loc[i]);
+			if(!(stat_loc[i] & 0x00FF))
+			{
+				printf("\nA child with pid %d terminated with exit code %d\n", pid, stat_loc[i]>>8);
+				
+				if(i==1)
+				exit(0);
+			}
+		}	
+			} 
+			 
+			 		
+			 //else 
+	  		 
+			 		
+		
 }
 
 void USRhandler(int signum)//the parent as child process send SIGUSR1 to its PID
 {
-	pid= wait(&stat_loc);
-	if(!(stat_loc & 0x00FF))
+	pid= wait(&flag1);
+	if(!(flag1 & 0x00FF))
 	{
-		if(stat_loc>>8 <n/2)
-			printf("child 1: Value found at position %d\n", stat_loc>>8);	
+		if(flag1>>8 <n/2)
+			printf("child 1: Value found at position %d\n", flag1>>8);	
 		else
-			printf("child 2: Value found at position %d\n", stat_loc>>8);
+			printf("child 2: Value found at position %d\n", flag1>>8);
 		for(int i=0;i<=children;i++)
 		kill(pidArr[i],SIGKILL);
 	}
